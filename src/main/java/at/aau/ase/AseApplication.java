11f89843d6a54a880435e3b781eeaf35bdf8c035
Fabian <oraze.fabian@icloud.com>
2021-12-30 08:39:19 +0100

    initial commit

---
+++ src/main/java/at/aau/ase/AseApplication.java
@@ -0,0 +1,13 @@
+package at.aau.ase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/aau/ase/AseApplicationTests.java
@@ -0,0 +1,13 @@
+package at.aau.ase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
