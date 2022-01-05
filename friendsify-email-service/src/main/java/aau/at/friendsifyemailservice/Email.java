fc03ce0e1e01cf001c199f4de74ec707755221a7
Larissa1702 <lakrainer@edu.aau.at>
2022-01-05 10:05:02 +0100

    added automatically create ID

--- friendsify-email-service/src/main/java/aau/at/friendsifyemailservice/Email.java
+++ friendsify-email-service/src/main/java/aau/at/friendsifyemailservice/Email.java
@@ -1,11 +1,14 @@
 package aau.at.friendsifyemailservice;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Email {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     int id;
 
     String from;
