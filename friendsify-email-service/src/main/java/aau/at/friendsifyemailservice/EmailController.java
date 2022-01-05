a7de40b4d490e7479f84a60c1d1b8e17904ac63e
Larissa1702 <lakrainer@edu.aau.at>
2022-01-05 11:04:59 +0100

    added functions: PostEmail (=create Email), PutEmail (=update Email), DeleteEmail (= delete email)

--- friendsify-email-service/src/main/java/aau/at/friendsifyemailservice/EmailController.java
+++ friendsify-email-service/src/main/java/aau/at/friendsifyemailservice/EmailController.java
@@ -3,14 +3,9 @@ package aau.at.friendsifyemailservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController()
 @RequestMapping("/emails")
@@ -46,5 +41,29 @@ public class EmailController {
 //    public ResponseEntity<Email> listEmails(@PathVariable("id") Long id){
 //        Email e = emailDAO.findById(id);
 //        return ResponseEntity.ok(e);
+//    }
+
+
+    @PostMapping()
+    public Email createEmail(@RequestBody Email email){
+        return emailDAO.save(email);
+    }
+
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Email> updateEmail(@PathVariable("id") Long id, @RequestBody Email email){
+//        Email originEmail = emailDAO.findById(id);
+//        originEmail.updateFromDTO(email);
+//        Email updatedEmail = emailDAO.save(originEmail);
+//        return ResponseEntity.ok(updatedEmail);
+//    }
+
+//    @DeleteMapping("/{id}")
+//    public Map<String, Boolean> deleteEmail(@PathVariable("id") Long id){
+//        Email originEmail = emailDAO.findById(id);
+//        emailDAO.delete(originEmail);
+//        Map<String, Boolean>  map = new HashMap<>();
+//        map.put("deleted", Boolean.TRUE);
+//        return map;
+//
 //    }
 }
