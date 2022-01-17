c3a988c4044bdd2673694928a8a059b1cd954252
Fabian <oraze.fabian@icloud.com>
2022-01-17 19:54:10 +0100

    small fixes to init script for person table

--- friendsify-email-service/src/main/java/aau/at/friendsifyemailservice/EmailController.java
+++ friendsify-email-service/src/main/java/aau/at/friendsifyemailservice/EmailController.java
@@ -1,7 +1,6 @@
 package aau.at.friendsifyemailservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 @RestController()
