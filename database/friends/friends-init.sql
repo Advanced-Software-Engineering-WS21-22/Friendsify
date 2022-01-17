8845392e622fb2cdef5932d020a554d7493abdb7
Fabian <oraze.fabian@icloud.com>
2022-01-17 14:06:58 +0100

    fixed error in friends-init.sql regarding multiple primary keys

--- database/friends/friends-init.sql
+++ database/friends/friends-init.sql
@@ -1,9 +1,10 @@
 CREATE TABLE friends
 (
-    EMAIL_P_INITIATOR varchar primary key not null,
-    EMAIL_P_FRIEND    varchar primary key not null,
+    EMAIL_P_INITIATOR varchar  not null,
+    EMAIL_P_FRIEND    varchar  not null,
     FS_START_DATE     date                not null,
-    IS_TIMED_OUT      bool
+    IS_TIMED_OUT      bool,
+    PRIMARY KEY (EMAIL_P_FRIEND, EMAIL_P_INITIATOR)
 );
 
 INSERT INTO friends(EMAIL_P_INITIATOR, EMAIL_P_FRIEND, FS_START_DATE, IS_TIMED_OUT)
