7d6c146819305ea15ba77a146f36bd6f92511940
Alexander Hofer <alehofer@edu.aau.at>
2022-01-19 21:17:13 +0100

    adding of module weather service

---
+++ friendsify-weather-service/src/test/java/aau/at/friendsify/weather/service/AppTests.java
@@ -0,0 +1,13 @@
+package aau.at.friendsify.weather.service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled
+public class AppTests {
+
+    @Test
+    public void testIt() {
+
+    }
+}
