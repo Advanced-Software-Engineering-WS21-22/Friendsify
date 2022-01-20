51eb2e8d8392637c74b8036a6d8f3638eee128d7
Larissa1702 <lakrainer@edu.aau.at>
2022-01-20 09:02:27 +0100

    added new package for exception and moved all exceptions into it

--- friendsify-friends-service/src/main/java/aau/at/friendsifyfriendsservice/exceptions/ResourceNotFoundException.java
+++ friendsify-friends-service/src/main/java/aau/at/friendsifyfriendsservice/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package aau.at.friendsifyfriendsservice;
+package aau.at.friendsifyfriendsservice.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
