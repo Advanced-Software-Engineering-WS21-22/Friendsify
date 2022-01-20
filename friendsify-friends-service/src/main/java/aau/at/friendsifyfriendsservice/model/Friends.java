ff06b1c22318161c6766fdd82f64f53541d986b0
Larissa1702 <lakrainer@edu.aau.at>
2022-01-20 09:30:51 +0100

    changed name of variables, because of an error (varaiblename has to be the same as column name in table)

--- friendsify-friends-service/src/main/java/aau/at/friendsifyfriendsservice/model/Friends.java
+++ friendsify-friends-service/src/main/java/aau/at/friendsifyfriendsservice/model/Friends.java
@@ -8,9 +8,7 @@ import java.sql.Date;
 public class Friends {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private boolean timed_out;
+    private Long id_friend;
 
     @Email
     private String email_p_initiator;
@@ -18,33 +16,35 @@ public class Friends {
     @Email
     private String email_p_friend;
 
-    Date fs_start_date;
+    private Date fs_start_date;
+
+    private boolean is_timed_out;
 
     public Friends() {
     }
 
-    public Friends(Long id, boolean timed_out, String email_p_initiator, String email_p_friend, Date fs_start_date) {
-        this.id = id;
-        this.timed_out = timed_out;
+    public Friends(Long id_friend, boolean is_timed_out, String email_p_initiator, String email_p_friend, Date fs_start_date) {
+        this.id_friend = id_friend;
+        this.is_timed_out = is_timed_out;
         this.email_p_initiator = email_p_initiator;
         this.email_p_friend = email_p_friend;
         this.fs_start_date = fs_start_date;
     }
 
-    public Long getId() {
-        return id;
+    public Long getId_friend() {
+        return id_friend;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setId_friend(Long id) {
+        this.id_friend = id;
     }
 
-    public boolean isTimed_out() {
-        return timed_out;
+    public boolean isIs_timed_out() {
+        return is_timed_out;
     }
 
-    public void setTimed_out(boolean timed_out) {
-        this.timed_out = timed_out;
+    public void setIs_timed_out(boolean timed_out) {
+        this.is_timed_out = timed_out;
     }
 
     public String getEmail_p_initiator() {
@@ -75,7 +75,7 @@ public class Friends {
         this.setEmail_p_initiator(other.getEmail_p_initiator());
         this.setEmail_p_friend(other.getEmail_p_friend());
         this.setFs_start_date(other.getFs_start_date());
-        this.setTimed_out(other.isTimed_out());
+        this.setIs_timed_out(other.isIs_timed_out());
     }
 
     @Override
@@ -84,7 +84,7 @@ public class Friends {
                 "id_p_initiator='" + email_p_initiator + '\'' +
                 ", id_p_friend='" + email_p_friend + '\'' +
                 ", fs_start_date='" + fs_start_date + '\'' +
-                ", timed_out='" + timed_out + '\'' +
+                ", timed_out='" + is_timed_out + '\'' +
                 '}';
     }
 }
