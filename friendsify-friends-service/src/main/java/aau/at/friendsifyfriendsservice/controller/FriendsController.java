3a24e8cff17f3f39e28d1b12352f455f2cc0845c
Larissa1702 <lakrainer@edu.aau.at>
2022-01-20 08:49:58 +0100

    checked for duplicate Entry in DB and also check for Friendship to itself (=not allowed)

--- friendsify-friends-service/src/main/java/aau/at/friendsifyfriendsservice/controller/FriendsController.java
+++ friendsify-friends-service/src/main/java/aau/at/friendsifyfriendsservice/controller/FriendsController.java
@@ -23,12 +23,12 @@ public class FriendsController {
     private FriendsDao friendsDao;
 
     @GetMapping()
-    public List<Friends> listLectures() {
+    public List<Friends> listFriendships() {
         return friendsDao.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Friends> getLectureById(@PathVariable("id") Long id)
+    public ResponseEntity<Friends> getFriendshipById(@PathVariable("id") Long id)
             throws ResourceNotFoundException {
 
         Friends f = friendsDao.findById(id)
@@ -37,19 +37,29 @@ public class FriendsController {
     }
 
     @PostMapping()
-    public Friends createLecture(@Valid @RequestBody Friends friends) {
-
+    public Friends createFriendship(@Valid @RequestBody Friends friends) throws Exception{
+        if(checkForDuplicate(friends)){
+            throw new Exception("Friendship is already in DB.");
+        }else  if(checkFriendshipToItself(friends)){
+            throw  new Exception("Friendship to itself is not allowed.");
+        }
         return friendsDao.save(friends);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Friends> updateLecture(@PathVariable("id") Long id,
-                                                 @Valid @RequestBody Friends friendsDto)
-            throws ResourceNotFoundException{
+    public ResponseEntity<Friends> updateFriendship(@PathVariable("id") Long id,
+                                                    @Valid @RequestBody Friends friendsDto)
+            throws ResourceNotFoundException, Exception{
 
 
         Friends origLecture = friendsDao.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Friendship not found for id: " + id));
+
+        if(checkForDuplicate(friendsDto)){
+            throw new Exception("Friendship is already in DB.");
+        }else if(checkFriendshipToItself(friendsDto)){
+            throw  new Exception("Friendship to itself is not allowed.");
+        }
 
         origLecture.updateFromDto(friendsDto);
 
@@ -68,6 +78,21 @@ public class FriendsController {
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
         return response;
+    }
+
+    private boolean checkForDuplicate(Friends friends){
+        for(Friends f: friendsDao.findAll()){
+            if(f.getEmail_p_initiator().equals(friends.getEmail_p_initiator()) && f.getEmail_p_friend().equals(friends.getEmail_p_friend())){
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean checkFriendshipToItself(Friends friends){
+        if(friends.getEmail_p_initiator().equals(friends.getEmail_p_friend())){
+            return true;
+        }
+        return false;
     }
 
 }
