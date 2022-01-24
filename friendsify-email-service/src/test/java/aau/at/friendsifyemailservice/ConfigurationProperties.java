e58c5cb6e721e6957030f36d99362e790452d9db
Fabian <oraze.fabian@icloud.com>
2022-01-24 14:52:27 +0100

    added unit tests for email service module

---
+++ friendsify-email-service/src/test/java/aau/at/friendsifyemailservice/ConfigurationProperties.java
@@ -0,0 +1,10 @@
+package aau.at.friendsifyemailservice;
+
+public class ConfigurationProperties {
+    public static final String SAMPLE_RECEIVER_GMAIL = "sample@gmail.com";
+    public static final String SAMPLE_SUBJECT = "sample-subject";
+    public static final String SAMPLE_TEXT = "sample text";
+    public static final String SEND_FROM_USER_TEXT = "\n\nSend from user: ";
+    public static final String SAMPLE_SENDER_GMAIL = "sender@gmail.com";
+
+}
