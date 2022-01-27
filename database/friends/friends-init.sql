9de67d44efa2bda337716f1392fb464c3a45b05a
Larissa1702 <lakrainer@edu.aau.at>
2022-01-27 10:44:22 +0100

    updated friends-init.sql file, for auto incrementation of indices

--- database/friends/friends-init.sql
+++ database/friends/friends-init.sql
@@ -1,11 +1,11 @@
 CREATE TABLE friends
 (
-    ID_FRIEND         bigint primary key not null,
+    ID_FRIEND         bigint primary key not null AUTO_INCREMENT,
     EMAIL_P_INITIATOR varchar         not null,
     EMAIL_P_FRIEND    varchar         not null,
     FS_START_DATE     date            not null,
     IS_TIMED_OUT      bool
-);
+)DEFAULT AUTO_INCREMENT=5;
 
 INSERT INTO friends(ID_FRIEND, EMAIL_P_INITIATOR, EMAIL_P_FRIEND, FS_START_DATE, IS_TIMED_OUT)
 VALUES (0, 'hans.m@gmail.com', 'anna@mustermann.de', '2019-01-31', false),
