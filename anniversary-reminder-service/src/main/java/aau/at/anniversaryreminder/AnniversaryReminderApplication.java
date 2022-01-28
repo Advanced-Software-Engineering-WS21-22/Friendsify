9889b6edb5f08cbf360342bffceaa0a73b81efb0
Fabian <oraze.fabian@icloud.com>
2022-01-28 11:18:19 +0100

    created new module, added service inside docker-compose and made a dockerfile

---
+++ anniversary-reminder-service/src/main/java/aau/at/anniversaryreminder/AnniversaryReminderApplication.java
@@ -0,0 +1,13 @@
+package aau.at.anniversaryreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnniversaryReminderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnniversaryReminderApplication.class, args);
+    }
+
+}
---
+++ anniversary-reminder-service/src/test/java/aau/at/anniversaryreminder/AnniversaryReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package aau.at.anniversaryreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnniversaryReminderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
