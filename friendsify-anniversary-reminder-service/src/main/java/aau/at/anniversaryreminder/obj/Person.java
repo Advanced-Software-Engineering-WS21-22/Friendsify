c4af823a7af93e899edfac3d2c09fc17654fc269
Fabian <oraze.fabian@icloud.com>
2022-02-02 10:46:52 +0100

    added plain objects into module specifying friends and person objects

---
+++ friendsify-anniversary-reminder-service/src/main/java/aau/at/anniversaryreminder/obj/Person.java
@@ -0,0 +1,27 @@
+package aau.at.anniversaryreminder.obj;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    private LocalDate birthday;
+
+    private String email;
+
+}
