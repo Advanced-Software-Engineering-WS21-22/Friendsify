fcc8e75a887f88ec4cbe63239439485be21def97
Fabian <oraze.fabian@icloud.com>
2022-02-02 10:47:56 +0100

    implemented services that offer the basic logic for the anniversary module and the rest calls to the other needed services

---
+++ friendsify-anniversary-reminder-service/src/main/java/aau/at/anniversaryreminder/service/FriendsServiceImpl.java
@@ -0,0 +1,69 @@
+package aau.at.anniversaryreminder.service;
+
+import aau.at.anniversaryreminder.obj.Friend;
+import aau.at.friendsifybirthdayservice.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+@Service
+public class FriendsServiceImpl implements FriendsService {
+
+    private final RestTemplate rt;
+
+    @Value("${friend.host.url}")
+    private String host;
+
+    public FriendsServiceImpl() {
+        this.rt = new RestTemplateBuilder().build();
+    }
+
+    public FriendsServiceImpl(String host) {
+        this();
+        this.host = host;
+    }
+
+    @Override
+    public List<Friend> getFriendsOf(String emailInitiator) throws ResourceNotFoundException {
+        String filter = "?email_initiator={email}";
+        Friend[] friends;
+        try {
+            friends = this.rt.getForObject(host + filter, Friend[].class, emailInitiator);
+        } catch (HttpClientErrorException e) {
+            throw new ResourceNotFoundException();
+        }
+        return Arrays.asList(friends);
+    }
+
+    @Override
+    public boolean areTheyFriends(String emailInitiator, String emailPossibleFriend) throws ResourceNotFoundException {
+        String filter = "?email_initiator={email}";
+        Stream<Friend> friends;
+        try {
+            friends = Arrays.stream(this.rt.getForObject(host + filter, Friend[].class, emailInitiator)).filter(friendShip -> friendShip.getEmailFriend().equals(emailPossibleFriend));
+        } catch (HttpClientErrorException e) {
+            throw new ResourceNotFoundException();
+        }
+        return friends.findAny().isPresent();
+    }
+
+    @Override
+    public LocalDate getFriendshipStartDate(String emailInitiator, String emailFriend) throws ResourceNotFoundException {
+        String filter = "?email_initiator={emailInitiator}&email_friend={emailFriend}";
+        Friend[] friends;
+        try {
+            friends = this.rt.getForObject(host + filter, Friend[].class, emailInitiator, emailFriend);
+        } catch (HttpClientErrorException e) {
+            throw new ResourceNotFoundException();
+        }
+        return Objects.requireNonNull(friends)[0].getFriendshipStartDate();
+    }
+}
---
+++ friendsify-anniversary-reminder-service/src/main/java/aau/at/anniversaryreminder/service/PersonServiceImpl.java
@@ -0,0 +1,42 @@
+package aau.at.anniversaryreminder.service;
+
+import aau.at.anniversaryreminder.obj.Friend;
+import aau.at.friendsifybirthdayservice.exception.ResourceNotFoundException;
+import aau.at.friendsifybirthdayservice.obj.Person;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
+
+@Service
+public class PersonServiceImpl implements PersonService {
+
+    private final RestTemplate rt;
+
+    @Value("${person.host.url}")
+    private String host;
+
+    public PersonServiceImpl() {
+        this.rt = new RestTemplateBuilder().build();
+    }
+
+    public PersonServiceImpl(String host) {
+        this();
+        this.host = host;
+    }
+
+    @Override
+    public String getNameByEmail(String email) {
+        String filter = "?email={email}";
+        Person[] persons;
+        try {
+            persons = this.rt.getForObject(host + filter, Person[].class, email);
+        } catch (HttpClientErrorException e) {
+            throw new ResourceNotFoundException();
+        }
+        return Objects.requireNonNull(persons)[0].getFullName();
+    }
+}
